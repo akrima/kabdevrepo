75100438dadb38ab039d45cd22b33402f4de19aa
akrima <stayak1@free.fr>
2016-09-18 11:34:10 -0300

    Update Venza.java

--- venza/src/org/cc/krima/main/Venza.java
+++ venza/src/org/cc/krima/main/Venza.java
@@ -1,7 +1,7 @@
 package org.cc.krima.main;
 
 public class Venza {
-	// C'est la méthode main du projet Venza
+	// C'est la méthode main du projet Venza et de la classe Venza
 	public static void main(String[] args) {
 	 System.out.println("Je suis une classe de test seulement!");
 	}
